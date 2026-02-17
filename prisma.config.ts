cfa5f2a54104cfadfa149effd72842b91fb0e686
Kamal Mudgil <kamalmudgil02@users.noreply.github.com>
2026-02-17 22:31:08 +0530

    Fix prisma config and remove .env

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,13 +2,13 @@ import { defineConfig } from '@prisma/config';
 import 'dotenv/config';
 
 export default defineConfig({
-  earlyAccess: true,
+  // earlyAccess is not supported in this version
   schema: 'prisma/schema.prisma',
   migrations: {
     seed: 'ts-node --compiler-options {"module":"CommonJS"} prisma/seed.ts',
   },
   datasource: {
-    provider: 'postgresql',
+    // provider is defined in schema.prisma and not needed / allowed here in this context
     url: process.env.DATABASE_URL!,
   },
 });
